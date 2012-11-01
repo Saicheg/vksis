1674f18bfcdaee796b08047d8700944eb14b3b7f
Valentine Zavadsky <saicheg@gmail.com>
2012-11-01 19:09:16 +0300

    working example with shared memory

--- 1/c/lab01.cpp
+++ 1/c/lab01.cpp
@@ -7,16 +7,18 @@ int main(int argc, char** argv) {
   string mode = argv[1];
 
   if (mode == "slave") {
-    if (argc < 3) usage(argv[1]);
-
-    fd = open(argv[2], O_RDWR);
+    char* name = (char*)calloc(1024, sizeof(char));
+    read_name(name);
+    fd = open(name, O_RDWR);
     if (fd == -1) {
       cerr << "error opening file" << std::endl;
       return -1;
     }
 
   }else if (mode=="master") {
-    fd = open("/dev/ptmx", O_RDWR | O_NOCTTY);
+
+    // fd = open("/dev/ptmx", O_RDWR | O_NOCTTY);
+    fd = open(argv[2], O_RDWR | O_NOCTTY);
     if (fd == -1) {
       cerr << "error opening file" << std::endl;
       return -1;
@@ -24,11 +26,30 @@ int main(int argc, char** argv) {
     grantpt(fd);
     unlockpt(fd);
     char* pts_name = ptsname(fd);
-    cerr << "ptsname: " << pts_name << std::endl;
+    cerr << "ptsname: " << pts_name << endl;
+    write_name(pts_name);
   } else {
     usage(argv[1]);
   }
 
+  configure_com(fd);
+
+  /* start reader thread */
+  pthread_t thread;
+  pthread_create(&thread, 0, reader_thread, (void*) &fd);
+
+  /* read from stdin and send it to the serial port */
+  char c;
+  while (true) {
+    cin >> c;
+    write(fd, &c, 1);
+  }
+
+  close(fd);
+  return 0;
+}
+
+void configure_com(int fd) {
   /* serial port parameters */
   struct termios newtio;
   memset(&newtio, 0, sizeof(newtio));
@@ -47,23 +68,6 @@ int main(int argc, char** argv) {
   cfsetispeed(&newtio, BAUDRATE);
   cfsetospeed(&newtio, BAUDRATE);
   tcsetattr(fd, TCSANOW, &newtio);
-
-  /* start reader thread */
-  pthread_t thread;
-  pthread_create(&thread, 0, reader_thread, (void*) &fd);
-
-  /* read from stdin and send it to the serial port */
-  char c;
-  while (true) {
-    cin >> c;
-    write(fd, &c, 1);
-  }
-
-  close(fd);
-  return 0;
-}
-
-void configure_com(int* fd) {
 }
 
 void* reader_thread(void* pointer) {
@@ -78,8 +82,33 @@ void* reader_thread(void* pointer) {
 }
 
 void usage(char* cmd) {
-  cerr << "usage: " << cmd << " slave|master [device, only in slave mode]" << std::endl;
+  cerr << "usage: " << cmd << "slave|master" << endl;
   exit(1);
+}
+
+void write_name(char* id) {
+  int len = strlen(id);
+
+  char* point = (char*)calloc(2, sizeof(int));
+  int shmid = shmget(key, SHM_SIZE, IPC_CREAT | S_IRUSR | S_IWUSR);
+
+  if ((point = (char*)shmat(shmid, 0, 0)) == (char*) -1) {
+    cerr << "write shmat error" << endl;
+    exit(1);
+  }
+  strcpy(point, id);
+  shmdt(point);
+}
+
+int read_name(char* name) {
+  char* point;
+  int shmid = shmget(key, SHM_SIZE, S_IRUSR | S_IWUSR);
+  // Check if availible to get shared memory
+  if ((point = (char*)shmat(shmid, 0, 0)) == (char*) -1) {
+    return -1;
+  }
+  strcpy(name, point);
+  return 1;
 }
 
 
--- 1/c/lab01.h
+++ 1/c/lab01.h
@@ -8,10 +8,19 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <termios.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
 
 #define BAUDRATE B9600
+#define SHM_SIZE 1024
+// #define SHARED_EXISTS 42
+key_t key = 1024;
 
 using namespace std;
 
 void usage(char* cmd);
 void* reader_thread(void* pointer);
+void configure_com(int fd);
+// void write_name(char* id);
+// int read_name(char* name);
